b1af3bedaaeb4e2cd7be07508c2aac7e17df48c8
Steve Harmeyer <harmeyersteve@gmail.com>
2023-02-17 09:17:21 -0700

    corporate site framework

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="container py-5">
+            <slot />
+        </div>
+        <Footer />
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxt/content',
+    ],
+    ssr: false,
+    css: ['~/assets/css/main.scss'],
+});
